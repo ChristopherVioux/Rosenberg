d7e96d07c08dfcab06c38688d1e3cf35fd44dc89
ChristopherVioux <christophervioux@gmail.com>
2022-12-04 14:09:50 +0100

    questions added

--- rosenberg/rosenberg.js
+++ rosenberg/rosenberg.js
@@ -112,34 +112,34 @@ function Question(title, answers, answerCorrect) {
 // Partie Création des mes données de Questions :
 let quiz = new Quiz();
 
-let question1 = new Question("Quel est l'énergie la plus utilisé au monde ? ", ["Le Gaz", "Le Pétrole", "Le Nucléaire"], 2);
+let question1 = new Question("Je pense que je suis une personne de valeur, au moins égale à n'importe qui d'autre", ["Tout à fait en désaccord", "Plutôt en désaccord", "Plutôt en accord", "Tout à fait en accord"], 2);
 quiz.addQuestion(question1);
 
-let question2 = new Question("Quel énergie pollue le plus ? ", ["Le Charbon", "Le Pétrole", "Le Nucléaire"], 1);
+let question2 = new Question("Je pense que je possède un certain nombre de belles qaulités", ["Tout à fait en désaccord", "Plutôt en désaccord", "Plutôt en accord", "Tout à fait en accord"], 1);
 quiz.addQuestion(question2);
 
-let question3 = new Question("Quel est le prix d'1 kWh en 2022 ? ", ["1,456€", "0,5156€", "0,139€"], 3);
+let question3 = new Question("Tout bien considéré, je suis porté à me considérer comme un raté", ["Tout à fait en désaccord", "Plutôt en désaccord", "Plutôt en accord", "Tout à fait en accord"], 3);
 quiz.addQuestion(question3);
 
-let question4 = new Question("Quel est le rendement d'un barrage hydraulique ? (aménagement important (>100 MW)) ", ["Varie entre 70 à 85%", "Varie entre 35 à 48%", "Varie entre 60 à 75%"], 1);
+let question4 = new Question("Je suis capable de faire les choses aussi bien que la majorité des gens", ["Tout à fait en désaccord", "Plutôt en désaccord", "Plutôt en accord", "Tout à fait en accord"], 1);
 quiz.addQuestion(question4);
 
-let question5 = new Question("En 2020 combien la France a t-elle importé d'uranium ? ", ["10 000 tonnes", "6 000 tonnes", "15 854 tonnes"], 2);
+let question5 = new Question("Je sens peu de raisons d'être fier de moi", ["Tout à fait en désaccord", "Plutôt en désaccord", "Plutôt en accord", "Tout à fait en accord"], 2);
 quiz.addQuestion(question5);
 
-let question6 = new Question("Combien existe t-il de réacteurs nucleaire en france ? ", ["52", "54", "56"], 3);
+let question6 = new Question("J'ai une attitude positive viv-à-vis de moi-même", ["Tout à fait en désaccord", "Plutôt en désaccord", "Plutôt en accord", "Tout à fait en accord"], 3);
 quiz.addQuestion(question6);
 
-let question7 = new Question("Quel énergie est utilisé (dans le film) pour l'armure d'Iron Man ? ", ["De l'antimatière", "Des particules Beta", "De l'Uranium 235"], 2);
+let question7 = new Question("Dans l'ensemble, je suis satisfait de moi", ["Tout à fait en désaccord", "Plutôt en désaccord", "Plutôt en accord", "Tout à fait en accord"], 2);
 quiz.addQuestion(question7);
 
-let question8 = new Question("Quand a été trouvé le gaz ? ", ["En 1752", "En 1815", "En 1911"], 2);
+let question8 = new Question("J'aimerais avoir plus de respect pour moi-même", ["Tout à fait en désaccord", "Plutôt en désaccord", "Plutôt en accord", "Tout à fait en accord"], 2);
 quiz.addQuestion(question8);
 
-let question9 = new Question("Quand a été trouvé l'uranium ? ", ["En 1789", "En 1864", "En 1920"], 1);
+let question9 = new Question("Parfois je me sens vraiment inutile", ["Tout à fait en désaccord", "Plutôt en désaccord", "Plutôt en accord", "Tout à fait en accord"], 1);
 quiz.addQuestion(question9);
 
-let question10 = new Question("Comment se nomme l'accélérateur de particules Franco-Suisse ? ", ["Le Cern", "Iter", "LHC"], 3);
+let question10 = new Question("Il m'arrive de penser que je suis un bon à rien", ["Tout à fait en désaccord", "Plutôt en désaccord", "Plutôt en accord", "Tout à fait en accord"], 3);
 quiz.addQuestion(question10);
 
 
